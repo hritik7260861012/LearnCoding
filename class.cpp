f471702e697c07ef30751deb62fc31849d356473
hritik7260861012 <ritiksingh53753@gmail.com>
2025-01-11 22:42:46 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class hello{
+    public:
+    void world(){
+        cout<<"hello world"<<endl;
+    }
+
+};
+int main(){
+hello helloWorld;
+    helloWorld.world();
+return 0;
+
+}
